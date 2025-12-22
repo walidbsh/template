e35e6e36c474a9e6c7c8ba67c458a16cb9e4c8d0
walidbsh <164235427+walidbsh@users.noreply.github.com>
2025-12-22 13:25:50 +0100

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="led-screen">
+    <div 
+      v-for="(row, rowIndex) in screen" 
+      :key="rowIndex" 
+      class="led-row"
+    >
+      <span 
+        v-for="(dot, colIndex) in row" 
+        :key="colIndex" 
+        :class="['led-dot', { 'on': dot === 1 }]"
+        
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const rows = 32;
+    const cols = 16;
+
+    // create 2D array filled with 0
+    const screen = ref(Array.from({ length: rows }, () => new Array(cols).fill(0)));
+
+    // Example: turn some dots on
+
+    return { screen };
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+.led-screen {
+  display: inline-block;
+  background: #222;
+  padding: 10px;
+  border-radius: 8px;
+}
+
+.led-row {
+  display: flex;
+}
+
+.led-dot {
+  width: 15px;
+  height: 15px;
+  margin: 2px;
+  border-radius: 50%;
+  background-color: #444; /* off state */
+  transition: background-color 0.2s;
+}
+
+.led-dot.on {
+  background-color: #0f0; /* on state */
+}
+</style>
